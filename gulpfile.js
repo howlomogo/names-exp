c524f3275c2436007907c9a71fcf26ad9ead3fed
Matt Pilcher <matt.pilcher@akerolabs.com>
2015-11-23 15:59:50 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+
+// Default
+gulp.task('default', ['browser-sync', 'sass-watch', 'html-reload']);
+
+// Browser Sync
+gulp.task('browser-sync', function() {
+  console.log("Browser sync started");
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+});
+
+// HTML - Watch
+gulp.task('html-reload', function() {
+  gulp.watch('./*.html').on('change', browserSync.reload);
+});
+
+// SASS
+gulp.task('sass', function() {
+  console.log("Converting SASS to CSS")
+  gulp.src('css/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream());
+});
+
+// SASS - Watch
+gulp.task('sass-watch', function() {
+  console.log("Watching for CSS changes");
+  gulp.watch('css/*.scss', ['sass']);
+});
---
+++ js/app.js
@@ -0,0 +1,32 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/profile.html', // Home Page
+        controller: 'MainCtrl'
+      }).
+      when('/profile', {
+        templateUrl: 'views/profile.html',
+        controller: 'MainCtrl'
+      }).
+      when('/deckbuilder', {
+        templateUrl: 'views/deckbuilder.html',
+        controller: 'DeckBuilderCtrl'
+      }).
+      otherwise({
+        redirectTo: '/404',
+        templateUrl: 'views/404.html',
+        controller: 'MainCtrl'
+      });
+  }]);
+
+app.controller('MainCtrl', function($scope) {
+  // Store fb user details in controller scope
+  $scope.facebookUser = {
+    firstName: "",
+    lastName: "",
+    picture: "http://api.adorable.io/avatars/60/abott@adorable.io.png"
+  };
+});
