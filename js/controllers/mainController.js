40cb4723067cdbe8a7c05a35c866768eea0d3996
Matt Pilcher <matt.pilcher@akerolabs.com>
2015-11-28 02:04:38 +0000

    Basic log in funtionality

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,14 +1,37 @@
 angular.module('app').controller('MainCtrl', ['$scope', function($scope) {
   $scope.testdata = [
-  {first_name: "Tom", last_name: "Green", gender: "m"},
-  {first_name: "Katie", last_name: "Black", gender: "f"},
-  {first_name: "John", last_name: "Cena", gender: "m"},
-  {first_name: "Jane", last_name: "Doe", gender: "f"},
-  {first_name: "Sally", last_name: "Jones", gender: "f"}
+    {first_name: "Tom", last_name: "Green", gender: "m"},
+    {first_name: "Katie", last_name: "Black", gender: "f"},
+    {first_name: "John", last_name: "Cena", gender: "m"},
+    {first_name: "Jane", last_name: "Doe", gender: "f"},
+    {first_name: "Sally", last_name: "Jones", gender: "f"}
   ];
 
-  // Make an input box th
-  $scope.activeUser = {};
-  $scope.blah = "GREEN";
-}]);
+  // Change all when database is implemented
+  // Do modals then add a log in modal
+  $scope.activeUser = "";
 
+  $scope.checkName = function() {
+    if(doesUserExist(this.login_name)) {
+      console.log("Logging in as " + this.login_name);
+      $scope.activeUser = this.login_name;
+    }
+    else {
+      console.log("User does not exist");
+    }
+  };
+
+  function doesUserExist(login_name) {
+    for (var i = 0; i < $scope.testdata.length; i++) {
+      if ($scope.testdata[i]['first_name'] === login_name) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  $scope.logout = function() {
+    console.log("Logging Out");
+    $scope.activeUser = "";
+  }
+}]);
